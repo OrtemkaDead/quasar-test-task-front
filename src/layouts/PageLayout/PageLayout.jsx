316dc7da71852dac114c9d25e370b667d87b0227
OrtemkaDead <allbuskk@gmail.com>
2022-07-13 14:12:38 +0500

    Убрал видимые отступы у свайпера и изменил layout страницы

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -3,14 +3,25 @@ import React from "react";
 // Styles
 import "./PageLayout.scss";
 
-const PageLayout = ({ classNameHeader, headerSlot, contentSlot }) => {
+const PageLayout = ({
+  classNameHeader,
+  classNameContent,
+  headerSlot,
+  contentSlot,
+}) => {
   return (
     <div className="page-layout">
       <header className={["page-layout__header", classNameHeader].join(" ")}>
         {headerSlot}
       </header>
 
-      <div className="page-layout__content container">{contentSlot}</div>
+      <div
+        className={["page-layout__content", classNameContent, "container"].join(
+          " "
+        )}
+      >
+        {contentSlot}
+      </div>
     </div>
   );
 };
